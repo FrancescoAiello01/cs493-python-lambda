406ebfce231c654f3877587cb9bb5f73b845e858
Francesco Aiello <aiellof@oregonstate.edu>
2021-02-25 11:30:26 -0800

    Get signed urls from s3 with structured json

--- app.py
+++ app.py
@@ -5,22 +5,38 @@ import json
 app = Flask(__name__)
 
 
+def add_to_music_dictionary(object_key, dictionary, signed_url):
+    object_key = object_key.split("/")
+    if object_key[0] not in dictionary:
+        dictionary[object_key[0]] = {object_key[1]: {object_key[2]: signed_url}}
+    elif object_key[1] not in dictionary[object_key[0]]:
+        dictionary[object_key[0]][object_key[1]]: {object_key[2]: signed_url}
+    else:
+        dictionary[object_key[0]][object_key[1]][object_key[2]] = signed_url
+
+    return dictionary
+
+
+def create_presigned_url(bucket_name, object_key, expiration, s3_client):
+    return s3_client.generate_presigned_url(
+        "get_object",
+        Params={"Bucket": bucket_name, "Key": object_key},
+        ExpiresIn=expiration,
+    )
+
+
 @app.route("/")
-def hello():
-    s3 = boto3.resource("s3")
+def get_music():
     s3_client = boto3.client("s3")
+    s3 = boto3.resource("s3")
 
-    # Your Bucket Name
     bucket = s3.Bucket("demo-s3-bucket-cs493-2")
+    bucket_content = s3_client.list_objects(Bucket="demo-s3-bucket-cs493-2")
 
-    # Gets the list of objects in the Bucket
-    s3_Bucket_iterator = bucket.objects.all()
+    music = {}
+    for object in bucket_content["Contents"]:
+        object_key = object["Key"]
+        signed_song_url = create_presigned_url(bucket.name, object_key, 500, s3_client)
+        music = add_to_music_dictionary(object_key, music, signed_song_url)
 
-    # Generates the Signed URL for each object in the Bucket
-    urls = [
-        s3_client.generate_presigned_url(
-            ClientMethod="get_object", Params={"Bucket": bucket.name, "Key": i.key}
-        )
-        for i in s3_Bucket_iterator
-    ]
-    return json.dumps(urls, indent=4)
+    return music
